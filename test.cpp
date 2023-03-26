da19f3343ffe51d05383e002ccdc0ab362c4a06f
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-03-26 19:41:40 +0100

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,24 @@
+void token::transfer( account_name from,
+                      account_name to,
+                      asset        quantity,
+                      string       memo )
+{
+    eosio_assert( from != to, "cannot transfer to self" );
+    eosio_assert( is_account( to ), "to account does not exist");
+    auto sym = quantity.symbol.name();
+    stats statstable( _self, sym );
+    const auto& st = statstable.get( sym );
+
+    require_recipient( from );
+    require_recipient( to );
+
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must transfer positive quantity" );
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    auto payer = has_auth( to ) ? to : from;
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, payer );
+}
